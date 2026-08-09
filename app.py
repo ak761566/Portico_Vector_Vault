b028c39468f53f59fa514df55fe86915a88217c6
ak761566 <ak761566@gmail.com>
2026-08-09 15:49:14 +0530

    Updated project name

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from torchgen.packaged.autograd.context import with_native_function_with_differe
 from rag_engine import RAGEngine
 
 # 1. Page Configuration
-st.set_page_config(page_title="Project Assistant", page_icon="", layout="centered")
-st.title("Ithaka (Portico) Project Assistant")
-st.caption("Grounded AI Q&A Engine powered by project Knowledge Base")
+st.set_page_config(page_title="PyRAG-Portico", page_icon="", layout="centered")
+st.title("Ithaka (Portico) Workbench Intelligence")
+#st.caption("Grounded AI Q&A Engine powered by project Knowledge Base")
 
 # 2. Lazy-load the backend engine using cache parameters
 # This ensures we only load the LLM and FAISS files ONCE, not on every keystroke.
